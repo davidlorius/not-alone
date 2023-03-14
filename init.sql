548feed6e493df83d58341094f90cb8febec6879
David Leon <david@lorius.cloud>
2023-03-13 18:37:00 -0600

    Dockerize project

---
+++ init.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE notalone_prod;
+CREATE DATABASE notalone_test;
+GRANT ALL PRIVILEGES ON DATABASE notalone_prod TO notuser;
+GRANT ALL PRIVILEGES ON DATABASE notalone_test TO notuser;
+GRANT ALL PRIVILEGES ON DATABASE notalone_development TO notuser;
