6d13fafe4d581adfe9a2de378a4a245918b01e3e
David Leon <david@lorius.cloud>
2023-04-01 16:09:24 -0600

    Deploy the docker image in the Lorius VPS

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Allow requests from domain 'notalone.adrianaprojects.com'
+  config.hosts << "notalone.adrianaprojects.com"
 end
